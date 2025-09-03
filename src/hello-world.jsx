25dedf9d4ca539206fc4a8cbe77693719b2b4934
sjmy <23250749+sjmy@users.noreply.github.com>
2025-09-03 12:47:57 -0700

    README update, homepage change

---
+++ src/hello-world.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function HelloWorld() {
+  return (
+    <>
+      <div className="card">
+        <h1>Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default HelloWorld;
